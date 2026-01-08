bfbbb7abb34db24022cc8ebb9322d54156187e02
meektion <168012408+meektion@users.noreply.github.com>
2026-01-08 20:19:10 +0800

    Add files via upload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,18 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 export default function Footer() {
+  const [year, setYear] = useState(new Date().getFullYear());
+
+  useEffect(() => {
+    setYear(new Date().getFullYear());
+  }, []);
+
   return (
     <footer className="w-full  h-1/12 text-center  bg-slate-200  flex flex-col justify-center items-center">
       <div >
-        <p className="text-xs text-gray-500">Copyright Ⓒ 2024 All rights reserved. 请勿上传违反中国法律的图片，违者后果自负。 本程序基于Cloudflare Pages，开源于
-          <Link 
-          href="https://github.com/meektion/img"
-          className="text-blue-300  hover:text-red-900 ml-1"
-          target="_blank"
-          rel="noopener noreferrer"
-          >GitHub Telegraph-Image</Link> </p>
+        <p className="text-xs text-gray-500">
+          © {year} Designed and developed by Jiang Sheng
+        </p>
       </div>
     </footer>
   );
